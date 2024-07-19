d87feee886691c3542c933358cf975f32b64a3cb
Musa <79163385+musayuksel@users.noreply.github.com>
2024-07-19 14:09:33 +0100

    Add another placeholder stack

---
+++ lib/another-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class AnotherStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Placeholder stack - no resources are defined here
+  }
+}
