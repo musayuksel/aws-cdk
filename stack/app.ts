36b6b1827c6c06482760ff4beb66da1d4f1e2e08
Musa <79163385+musayuksel@users.noreply.github.com>
2025-04-25 10:54:22 +0100

    Update app entry point for new structure

--- stack/app.ts
+++ stack/app.ts
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib'
-import { MusaApiLambdaCrudDynamoDBStack } from './crud-api-cdk-stack'
+import { MusaApiLambdaCrudDynamoDBStack } from './api/crud-api-cdk-stack'
+import { MusaCrudApiPipelineStack } from './pipeline/pipeline-stack'
 
 const app = new cdk.App()
+
+// Repository and connection details
+const repositoryOwner = 'musayuksel'
+const repositoryName = 'aws'
+const branchName = 'pipeline-setup'
+const connectionArn = 'arn:aws:codeconnections:eu-west-1:749144762306:connection/eb9218a5-7ef1-4bde-b7d2-15c98a33ec2d'
+const apiStackName = 'MusaApiLambdaCrudDynamoDBStack'
+
 // eslint-disable-next-line no-new
-new MusaApiLambdaCrudDynamoDBStack(app, 'musaApiLambdaCrudDynamoDBStack')
+new MusaApiLambdaCrudDynamoDBStack(app, apiStackName)
+
+new MusaCrudApiPipelineStack(app, 'MusaCrudApiPipelineStack', {
+  repositoryOwner,
+  repositoryName,
+  branchName,
+  connectionArn,
+  apiStackName
+})
+
+app.synth()
